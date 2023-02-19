aa09914795a2213bf85157ee27249adbac291fc3
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2023-02-19 00:06:48 -0500

    Created HeroService to fetch hero data

--- client/src/app/heroes/heroes.component.ts
+++ client/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +8,25 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = []; 
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {
+    // We don't call getHeroes() here, because it is bad practice to call a
+    // synchronous method in the constructor. The constructor shouldn't DO
+    // anything. It should just set the initial state of the component.
+    // Instead, we call getHeroes() in the ngOnInit lifecycle hook.
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  } 
 }
